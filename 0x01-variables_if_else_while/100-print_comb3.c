2c1c1c89ef9b5c6e45212dd24f2629d355f65404
KenanAllan <kenanallanwilliam@gmail.com>
2023-10-08 08:09:15 -0700

    f

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,14 +5,18 @@
  *
  * Return: zero
  */
-int main(void) {
+int main(void)
+{
 	int i, j;
 
-	for (i = 0; i <= 9; i++) {
-		for (j = i + 1; j <= 9; j++) {
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if (j != 89) {
+			if (j != 89)
+			{
 				putchar(',');
 				putchar(' ');
 			}
