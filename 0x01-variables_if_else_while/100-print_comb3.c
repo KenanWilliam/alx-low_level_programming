24d64d9331bbdd3e516eb94688f33947fec7f725
KenanAllan <kenanallanwilliam@gmail.com>
2023-10-08 07:46:14 -0700

    cmon

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - print 01 to 99
- *
- * Return: zero
- */
+ * main - print 00 to 99
+ *
+ * Return: zero
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 89; i++)
 	{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-		if (i != 99 && i % 10 != 0) {
-			putchar(',');
-			putchar(' ');
-		}
+		if (i == 89)
+			break;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
 }
-
