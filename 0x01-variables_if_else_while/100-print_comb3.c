70eb55e72641bcc6b8cd8d36b5ba0f94f377500c
KenanAllan <kenanallanwilliam@gmail.com>
2023-10-08 08:03:44 -0700

    g

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void) {
 		for (j = i + 1; j <= 9; j++) {
 			putchar(i + '0');
 			putchar(j + '0');
-			if (i, j != 8, 9) {
+			if (i && j != 89) {
 				putchar(',');
 				putchar(' ');
 			}
