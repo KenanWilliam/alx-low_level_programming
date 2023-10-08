c9e612131acfb90409e5da3aa9c7b611ea099a0f
KenanAllan <kenanallanwilliam@gmail.com>
2023-10-08 07:57:32 -0700

    qw

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-  int i, j;
+	int i, j;
 
-  for (i = 0; i <= 9; i++) {
-    for (j = i + 1; j <= 9; j++) {
-      putchar(i + '0');
-      putchar(j + '0');
-      putchar(',');
-      putchar(' ');
-    }
-  }
+	for (i = 0; i <= 9; i++) {
+		for (j = i + 1; j <= 9; j++) {
+			putchar(i + '0');
+			putchar(j + '0');
+			if (j != 9) {
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-  putchar('\n');
+	putchar('\n');
 
-  return 0;
+	return 0;
 }
 
