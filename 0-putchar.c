c2a4d9a9ea0a0255acfc958cb0646bed6e26172d
KenanAllan <kenanallanwilliam@gmail.com>
2023-10-10 23:46:37 -0700

    print putchar v2

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("_putchar\n");
-  return 0;
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
 }
 
